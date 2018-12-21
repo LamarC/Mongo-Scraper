ffc83ddd68e2f6398f6f731018a55efd0c26e0b1
LamarC <dviann89@gmail.com>
2018-12-21 01:09:38 -0600

    New dependencies added

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const exphbs = require("express-handlebars");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-const bodyParser = require(body - parser);
+const bodyParser = require("body-parser");
 
 //Scraping tools
 const axios = require("axios");
@@ -11,22 +11,32 @@ const cheerio = require("cheerio");
 //Require models
 const db = require("./models");
 
+const PORT = 3000;
+
+//Init express
 const app = express();
-
-//Do port
-
-//Set up static directory
-app.use(express.static("public"));
-
-//Parse app
-app.use(bodyParser.urlencoded({ extended: true }));
 
 //Use morgan to log req
 app.use(logger("dev"));
 
+//Parse req body as json
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Set up static directory
+app.use(express.static("public"));
+
+// Connect to the Mongo DB
+mongoose.connect(
+  "mongodb://localhost/createNamedb",
+  { useNewUrlParser: true }
+);
+
 //Set up handlebars
-
-const exphbs = require("express-handlebars");
-
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
+//Start Server
+app.listen(PORT, function() {
+  console.log("App listening on port " + PORT);
+});
