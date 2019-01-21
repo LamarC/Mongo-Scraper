70c4ac51e3a1e51198930a1d824877000809d431
LamarC <dviann89@gmail.com>
2019-01-20 19:11:23 -0600

    error fixed

--- server.js
+++ server.js
@@ -10,24 +10,28 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.use(logger("dev"));
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/";
+const MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 mongoose.connect(MONGODB_URI);
 
 const exphbs = require("express-handlebars");
-app.engine("hbs", exphbs({
-	defaultLayout: "main",
-	extname: ".hbs"
-}));
+app.engine(
+  "hbs",
+  exphbs({
+    defaultLayout: "main",
+    extname: ".hbs"
+  })
+);
 app.set("view engine", "hbs");
 
 require("./routes/routes.js")(app);
 
-app.listen(PORT, function () {
-    console.log("App listening on PORT " + PORT);
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
 });
